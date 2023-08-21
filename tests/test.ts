44e39c334c5749879c082dc88b872717e76fb7af
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-21 23:17:16 +0630

    Update tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,17 +1,30 @@
 import { strictEqual } from 'node:assert/strict';
+import { exec } from 'node:child_process';
 import { describe, it } from 'node:test';
+import { promisify } from 'node:util';
 
-import { execSync } from 'node:child_process';
+import packageJson from '../package.json' assert { type: 'json' };
 
-const runCli = (args: string) => {
-    return execSync(`node ./dist/bin/lib.js ${args}`).toString();
+const execAsync = promisify(exec);
+const packageVersion = packageJson.version;
+
+const runCli = async (args: string) => {
+    // execSync(`node ./dist/bin/lib.js ${args}`).toString();
+    const { stdout } = await execAsync(`node ./dist/bin/lib.js ${args}`);
+
+    return stdout.toString().trim();
 };
 
 void describe('Cli', () => {
-    void it(`should run 'help' command`, () => {
-        const result = runCli('--help');
-        const shouldContains = result.includes('--help  Shows a help message for this command in the console.');
+    void it(`should show 'help'`, async () => {
+        const result = await runCli('--help');
 
-        strictEqual(shouldContains, true);
+        strictEqual(result.includes('Show help'), true);
+    });
+
+    void it(`should show 'version'`, async () => {
+        const outputResult = await runCli('--version');
+
+        strictEqual(outputResult, packageVersion);
     });
 });
