12bfb7e2440039808c464e5dce3316ff64f11e69
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-21 18:03:25 +0630

    Add tests

---
+++ tests/test.ts
@@ -0,0 +1,17 @@
+import { strictEqual } from 'node:assert/strict';
+import { describe, it } from 'node:test';
+
+import { execSync } from 'node:child_process';
+
+const runCli = (args: string) => {
+    return execSync(`node ./dist/bin/lib.js ${args}`).toString();
+};
+
+void describe('Cli', () => {
+    void it(`should run 'help' command`, () => {
+        const result = runCli('--help');
+        const actual = result.includes('--help  Shows a help message for this command in the console.');
+
+        strictEqual(actual, true);
+    });
+});
