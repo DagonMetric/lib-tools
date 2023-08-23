045c4af34e15ac3acc6d3f96eefd11a5860b4c16
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-23 20:38:24 +0630

    Update cli build.ts

--- src/cli/commands/build.ts
+++ src/cli/commands/build.ts
@@ -16,42 +16,49 @@ export const command = 'build';
 export const describe = 'Build the project(s)';
 
 export function builder(argv: Argv): Argv<BuildCommandOptions> {
-    return (
-        argv
-            .usage('$0 build [options...]')
-            .example('$0 build', 'Build the project(s)')
-            .option('log-level', {
-                describe: 'Set logging level for output information',
-                choices: ['debug', 'info', 'warn', 'error', 'none'] as const,
-                default: 'info'
-            })
-            .option('version', {
-                describe: 'Set version to override the version field of the package.json file',
-                type: 'string'
-            })
-            .option('libconfig', {
-                describe: 'Set libconfig.json file location or set `auto` to analyze project structure automatically',
-                type: 'string'
-            })
-            .option('env', {
-                describe: 'Set environment name to override the task configuration with envOverrides options',
-                type: 'string'
-            })
-            .option('project', {
-                describe: 'Set project name to Filter project(s)',
-                type: 'string'
-            })
-
-            // TODO: To support watch
-            // .option('watch', {
-            //     describe: 'Run in watch mode.',
-            //     type: 'boolean'
-            // })
-            .option('help', {
-                describe: 'Show help for build command',
-                type: 'boolean'
-            })
-    );
+    return argv
+        .usage('$0 build [options...]')
+        .example('$0 build', 'Build the project(s)')
+        .option('logLevel', {
+            describe: 'Set logging level for output information',
+            choices: ['debug', 'info', 'warn', 'error', 'none'] as const
+        })
+        .option('libconfig', {
+            describe: 'Set libconfig.json file location',
+            type: 'string'
+        })
+        .option('project', {
+            describe: 'Set project name to Filter project(s)',
+            type: 'string'
+        })
+        .option('outputPath', {
+            describe: 'Set output directory for build results',
+            type: 'string'
+        })
+        .option('clean', {
+            describe: 'Set true to clean build output directory before emitting build results',
+            type: 'boolean'
+        })
+        .option('copy', {
+            describe: 'Set path to copy assets to output directory',
+            type: 'string'
+        })
+        .option('style', {
+            describe: 'Set SCSS or CSS file entry to compile or bundle',
+            type: 'string'
+        })
+        .option('script', {
+            describe: 'Set TypeScript or JavaScript file entry to compile or bundle',
+            type: 'string'
+        })
+        .option('version', {
+            describe: 'Set version to override the version field of the package.json file',
+            type: 'string'
+        })
+        .option('help', {
+            describe: 'Show help for build command',
+            type: 'boolean'
+        });
 }
 
 export async function handler(argv: ArgumentsCamelCase<BuildCommandOptions>): Promise<void> {
