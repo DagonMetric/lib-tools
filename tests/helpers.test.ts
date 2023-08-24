712e68780f8d0ec77f7397c3a294973e89a43ad3
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-24 17:44:08 +0630

    Update tests

--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -1,6 +1,7 @@
 import * as assert from 'node:assert';
 import { describe, it } from 'node:test';
 
+import { InvalidConfigError } from '../src/exceptions/index.js';
 import { BuildTaskConfig, ProjectConfig } from '../src/models/index.js';
 
 import { applyEnvOverrides } from '../src/helpers/apply-env-overrides.js';
@@ -91,6 +92,75 @@ void describe('Helpers', () => {
             };
 
             assert.deepStrictEqual(projectC, expectedConfig);
+        });
+
+        void it('should throw if cross extends found', () => {
+            const projects: Record<string, ProjectConfig> = {
+                a: {
+                    extends: 'c',
+                    tasks: {
+                        build: {
+                            banner: 'a',
+                            clean: true,
+                            copy: ['a.txt', 'b.txt']
+                        }
+                    }
+                },
+                b: {
+                    extends: 'a',
+                    tasks: {
+                        build: {
+                            clean: false,
+                            style: ['a.css']
+                        }
+                    }
+                },
+                c: {
+                    extends: 'b',
+                    tasks: {
+                        build: {
+                            banner: 'c',
+                            script: ['a.js']
+                        }
+                    }
+                }
+            };
+
+            const projectC = projects.c;
+
+            const expectedError = new InvalidConfigError('Cross referencing extend founds.', 'projects[c].extends');
+
+            assert.throws(() => applyProjectExtends('c', projectC, projects), expectedError);
+        });
+
+        void it('should throw if no base project to extend', () => {
+            const projects: Record<string, ProjectConfig> = {
+                a: {
+                    tasks: {
+                        build: {
+                            banner: 'a',
+                            clean: true,
+                            copy: ['a.txt', 'b.txt']
+                        }
+                    }
+                },
+                b: {
+                    extends: 'c',
+                    tasks: {
+                        build: {
+                            clean: false,
+                            style: ['a.css']
+                        }
+                    }
+                }
+            };
+
+            const projectB = projects.b;
+
+            assert.throws(
+                () => applyProjectExtends('b', projectB, projects),
+                'Invalid configuration. No base project to extends. Config location: projects[b].extends'
+            );
         });
     });
 });
