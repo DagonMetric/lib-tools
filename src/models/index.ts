3172ca7cbb8fe224f2b50a87e0b46ade625417ab
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-25 12:00:52 +0630

    Update models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,5 @@
 export * from './build-command-options.js';
 export * from './build-task-config.js';
-export * from './build-command-options.js';
 export * from './lib-config.js';
 export * from './overridable-task-config.js';
 export * from './project-config.js';
