0a08dd8dfbe5f665ef7da5b871152fdaa58863a8
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-26 11:16:52 +0630

    Update build handler

--- src/handlers/build/index.ts
+++ src/handlers/build/index.ts
@@ -1,9 +1,8 @@
 import { ParsedBuildTaskConfig } from '../../helpers/index.js';
 import { Logger } from '../../utils/index.js';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function runBuildTask(buildTaskConfig: ParsedBuildTaskConfig, logger: Logger): Promise<void> {
-    logger.error('Not Implemented.', JSON.stringify(buildTaskConfig));
+    logger.error(`Not Implemented. ${buildTaskConfig.taskName}`);
 
     return Promise.resolve();
 }
