52f27866e5ce772113137dd9bc6863d664dbfaa8
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-30 00:10:30 +0630

    Update exceptions

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -1,5 +1,3 @@
-export * from './internal-error.js';
 export * from './invalid-command-option-error.js';
 export * from './invalid-config-error.js';
-export * from './not-implemented-error.js';
 export * from './schema-validation-error.js';
--- src/exceptions/internal-error.ts
+++
@@ -1,5 +0,0 @@
-export class InternalError extends Error {
-    constructor(message = '') {
-        super(message);
-    }
-}
