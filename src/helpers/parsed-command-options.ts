7b42d4e70ce0fa9f0000208600fd7337c1d09029
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-30 16:43:01 +0630

    Update parsed-command-options.ts

--- src/helpers/parsed-command-options.ts
+++ src/helpers/parsed-command-options.ts
@@ -6,96 +6,86 @@ export interface ParsedCommandOptions extends CommandOptions {
     readonly _projects: string[];
     readonly _workspaceRoot: string | null;
     readonly _configPath: string | null;
+    readonly _env: Record<string, boolean>;
 
     // For build
-    readonly _env: Record<string, boolean>;
     readonly _outDir: string | null;
     readonly _copyEntries: string[];
     readonly _styleEntries: string[];
     readonly _scriptEntries: string[];
-}
-
-export class ParsedCommandOptionsImpl implements ParsedCommandOptions {
-    readonly _projects: string[];
-    readonly _workspaceRoot: string | null;
-    readonly _configPath: string | null;
-
-    // For build
-    readonly _env: Record<string, boolean>;
-    readonly _outDir: string | null;
-    readonly _copyEntries: string[];
-    readonly _styleEntries: string[];
-    readonly _scriptEntries: string[];
-
-    constructor(cmdOptions: CommandOptions) {
-        Object.assign(this, cmdOptions);
-
-        this._projects =
-            cmdOptions.project
-                ?.split(',')
-                .filter((projectName) => projectName && projectName.trim().length > 0)
-                .filter((value, index, array) => array.indexOf(value) === index) ?? [];
-
-        if (cmdOptions.workspace) {
-            const pathAbs = path.isAbsolute(cmdOptions.workspace)
-                ? path.resolve(cmdOptions.workspace)
-                : path.resolve(process.cwd(), cmdOptions.workspace);
-
-            if (path.extname(pathAbs)) {
-                this._configPath = pathAbs;
-                this._workspaceRoot = path.dirname(this._configPath);
-            } else {
-                this._configPath = null;
-                this._workspaceRoot = pathAbs;
-            }
-        } else {
-            this._configPath = null;
-            this._workspaceRoot = null;
-        }
-
-        this._env =
-            cmdOptions.env
-                ?.split(',')
-                .filter((envName) => envName && envName.trim().length > 0)
-                .filter((value, index, array) => array.indexOf(value) === index)
-                .reduce(
-                    (obj, key) => {
-                        return {
-                            ...obj,
-                            [key]: true
-                        };
-                    },
-                    {} as Record<string, boolean>
-                ) ?? {};
-
-        const workspaceRoot = this._workspaceRoot ?? process.cwd();
-
-        this._outDir = cmdOptions.outDir
-            ? path.isAbsolute(cmdOptions.outDir)
-                ? path.resolve(cmdOptions.outDir)
-                : path.resolve(workspaceRoot, cmdOptions.outDir)
-            : null;
-
-        this._copyEntries =
-            cmdOptions.copy
-                ?.split(',')
-                .filter((p) => p && p.trim().length > 0)
-                .filter((value, index, array) => array.indexOf(value) === index) ?? [];
-
-        this._styleEntries =
-            cmdOptions.style
-                ?.split(',')
-                .filter((p) => p && p.trim().length > 0)
-                .filter((value, index, array) => array.indexOf(value) === index) ?? [];
-
-        this._scriptEntries =
-            cmdOptions.script
-                ?.split(',')
-                .filter((p) => p && p.trim().length > 0)
-                .filter((value, index, array) => array.indexOf(value) === index) ?? [];
-    }
 }
 
 export function getParsedCommandOptions(cmdOptions: CommandOptions): ParsedCommandOptions {
-    return new ParsedCommandOptionsImpl(cmdOptions);
+    let workspaceRoot: string | null = null;
+    let configPath: string | null = null;
+
+    const projects =
+        cmdOptions.project
+            ?.split(',')
+            .filter((projectName) => projectName && projectName.trim().length > 0)
+            .filter((value, index, array) => array.indexOf(value) === index) ?? [];
+
+    if (cmdOptions.workspace) {
+        const pathAbs = path.isAbsolute(cmdOptions.workspace)
+            ? path.resolve(cmdOptions.workspace)
+            : path.resolve(process.cwd(), cmdOptions.workspace);
+
+        if (path.extname(pathAbs)) {
+            configPath = pathAbs;
+            workspaceRoot = path.dirname(configPath);
+        } else {
+            workspaceRoot = pathAbs;
+        }
+    }
+
+    const env =
+        cmdOptions.env
+            ?.split(',')
+            .filter((envName) => envName && envName.trim().length > 0)
+            .filter((value, index, array) => array.indexOf(value) === index)
+            .reduce(
+                (obj, key) => {
+                    return {
+                        ...obj,
+                        [key]: true
+                    };
+                },
+                {} as Record<string, boolean>
+            ) ?? {};
+
+    const outDir = cmdOptions.outDir
+        ? path.isAbsolute(cmdOptions.outDir)
+            ? path.resolve(cmdOptions.outDir)
+            : path.resolve(workspaceRoot ?? process.cwd(), cmdOptions.outDir)
+        : null;
+
+    const copyEntries =
+        cmdOptions.copy
+            ?.split(',')
+            .filter((p) => p && p.trim().length > 0)
+            .filter((value, index, array) => array.indexOf(value) === index) ?? [];
+
+    const styleEntries =
+        cmdOptions.style
+            ?.split(',')
+            .filter((p) => p && p.trim().length > 0)
+            .filter((value, index, array) => array.indexOf(value) === index) ?? [];
+
+    const scriptEntries =
+        cmdOptions.script
+            ?.split(',')
+            .filter((p) => p && p.trim().length > 0)
+            .filter((value, index, array) => array.indexOf(value) === index) ?? [];
+
+    return {
+        ...cmdOptions,
+        _projects: projects,
+        _workspaceRoot: workspaceRoot,
+        _configPath: configPath,
+        _env: env,
+        _outDir: outDir,
+        _copyEntries: copyEntries,
+        _scriptEntries: scriptEntries,
+        _styleEntries: styleEntries
+    };
 }
