b64142a94028bc1324f5a95df4af890313acf156
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-31 09:09:27 +0630

    Update utils

---
+++ src/utils/exec.ts
@@ -0,0 +1,18 @@
+import { spawn } from 'node:child_process';
+
+export async function exec(cmd: string): Promise<number> {
+    return new Promise((resolve, reject) => {
+        const proc = spawn(cmd, { stdio: 'inherit', shell: true });
+
+        proc.on('exit', (exitCode) => {
+            if (exitCode === 0) {
+                resolve(0);
+            } else {
+                reject(new Error(`Process exited with code: ${exitCode}`));
+            }
+        });
+        proc.on('error', (error) => {
+            reject(error);
+        });
+    });
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './colorize.js';
+export * from './exec.js';
 export * from './logger.js';
 export * from './path-helpers.js';
 export * from './read-json-with-comments.js';
