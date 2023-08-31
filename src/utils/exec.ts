2bfa6d4e939de0960145b5809d1f25f4c6453c82
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-31 15:35:08 +0630

    Update exec.ts

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -1,14 +1,21 @@
 import { spawn } from 'node:child_process';
 
+export class ExitCodeError extends Error {
+    constructor(readonly exitCode: number) {
+        super('');
+    }
+}
+
 export async function exec(cmd: string): Promise<void> {
     return new Promise((resolve, reject) => {
         const proc = spawn(cmd, { stdio: 'inherit', shell: true });
 
         proc.on('exit', (exitCode) => {
-            if (exitCode === 0) {
+            if (!exitCode || exitCode === 0) {
                 resolve();
+                return;
             } else {
-                reject(new Error(`Process exited with code: ${exitCode}`));
+                reject(new ExitCodeError(exitCode));
             }
         });
         proc.on('error', (error) => {
