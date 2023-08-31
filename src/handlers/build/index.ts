2281261cb6bb71528f5b13f3b0fc8e9e19a4cdd1
Moe Myat Zaw <mmzliveid@gmail.com>
2023-08-31 09:09:50 +0630

    Update handlers

--- src/handlers/build/index.ts
+++ src/handlers/build/index.ts
@@ -1,7 +1,7 @@
 import { ParsedBuildTask } from '../../helpers/index.js';
 import { Logger } from '../../utils/index.js';
 
-export function runBuildTask(buildTask: ParsedBuildTask, logger: Logger): Promise<void> {
+export default function (buildTask: ParsedBuildTask, logger: Logger): Promise<void> {
     logger.error(`Not Implemented. ${buildTask._taskName}`);
 
     return Promise.resolve();
