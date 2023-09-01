11e0787b3005d20a2e76260b8cff8768f74ca14a
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-01 15:26:57 +0630

    Update build task runner

--- src/handlers/build/index.ts
+++ src/handlers/build/index.ts
@@ -2,6 +2,7 @@ import { ParsedBuildTask } from '../../helpers/index.js';
 import { Logger } from '../../utils/index.js';
 
 import { getCleanTaskRunner } from './clean/index.js';
+import { getCopyTaskRunner } from './copy/index.js';
 
 export default async function (buildTask: ParsedBuildTask, logger: Logger): Promise<void> {
     const beforeCleanTaskRunner = getCleanTaskRunner(buildTask, logger, 'before', false);
@@ -9,6 +10,14 @@ export default async function (buildTask: ParsedBuildTask, logger: Logger): Prom
         const cleandPaths = await beforeCleanTaskRunner.run();
         if (cleandPaths.length) {
             logger.info(`Total ${cleandPaths.length} files or directories are cleaned.`);
+        }
+    }
+
+    const copyTaskRunner = getCopyTaskRunner(buildTask, logger, false);
+    if (copyTaskRunner) {
+        const copiedPaths = await copyTaskRunner.run();
+        if (copiedPaths.length) {
+            logger.info(`Total ${copiedPaths.length} files are copied.`);
         }
     }
 
