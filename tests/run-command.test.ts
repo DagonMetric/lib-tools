2dbc8f1996b28f7be59a9a1222b3418c9042fc90
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-06 23:44:39 +0630

    Update tests

--- tests/run-command.test.ts
+++ tests/run-command.test.ts
@@ -10,16 +10,23 @@ void describe('run command builder', () => {
     void it(`should parse command options`, async () => {
         const parser = yargs(hideBin(process.argv)).command(runCommand.command, '', runCommand.builder);
         const result = await parser.parseAsync(
-            'run build --env=prod,ci --project=a,b --clean=true --copy=README.md,**/*.md --style=styles.scss --script=index.ts',
+            'run build --logLevel=warn --workspace=tests/test-data --project=a,b --env=prod,ci --outDir=dist --clean=true --copy=README.md,**/*.md --style=styles.scss --script=index.ts --packageVersion=1.0.0',
             () => {
                 // Do nothing;
             }
         );
-        assert.strictEqual(result.env, 'prod,ci');
+
+        assert.strictEqual(result.task, 'build');
+        assert.strictEqual(result.logLevel, 'warn');
+        assert.strictEqual(result.workspace, 'tests/test-data');
         assert.strictEqual(result.project, 'a,b');
+        assert.strictEqual(result.env, 'prod,ci');
+
+        assert.strictEqual(result.outDir, 'dist');
         assert.strictEqual(result.clean, true);
         assert.strictEqual(result.copy, 'README.md,**/*.md');
         assert.strictEqual(result.style, 'styles.scss');
         assert.strictEqual(result.script, 'index.ts');
+        assert.strictEqual(result.packageVersion, '1.0.0');
     });
 });
