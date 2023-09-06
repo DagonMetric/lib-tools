3b5e6c8d51ea9dd9ba869168f179d2007f23741c
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-07 01:23:38 +0630

    Update tests

--- tests/command.test.ts
+++ tests/command.test.ts
@@ -2,26 +2,38 @@ import * as assert from 'node:assert';
 import { describe, it } from 'node:test';
 
 import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
 
 import * as runCommand from '../src/cli/commands/run.js';
 
-void describe('run command builder', () => {
+void describe('yargs.parseAsync', () => {
     void it(`should parse command options`, async () => {
-        const parser = yargs(hideBin(process.argv)).command(runCommand.command, '', runCommand.builder);
-        const result = await parser.parseAsync(
-            'run build --logLevel=warn --workspace=tests/test-data --project=a,b --env=prod,ci --outDir=dist --clean=true --copy=README.md,**/*.md --style=styles.scss --script=index.ts --packageVersion=1.0.0',
-            () => {
-                // Do nothing;
-            }
-        );
+        const cmdOptions = [
+            'run',
+            'build',
+            '--logLevel=warn',
+            '--workspace=tests/test-data',
+            '--project=a,b',
+            '--env=prod,ci',
+            '--outDir=dist',
+            '--clean=true',
+            '--copy=README.md,**/*.md',
+            '--style=styles.scss',
+            '--script=index.ts',
+            '--packageVersion=1.0.0'
+        ];
 
+        const result = await yargs()
+            .command(runCommand.command, runCommand.describe, runCommand.builder)
+            .parseAsync(cmdOptions, () => {
+                // Do nothing;
+            });
+
+        assert.strictEqual(result._[0], 'run');
         assert.strictEqual(result.task, 'build');
         assert.strictEqual(result.logLevel, 'warn');
         assert.strictEqual(result.workspace, 'tests/test-data');
         assert.strictEqual(result.project, 'a,b');
         assert.strictEqual(result.env, 'prod,ci');
-
         assert.strictEqual(result.outDir, 'dist');
         assert.strictEqual(result.clean, true);
         assert.strictEqual(result.copy, 'README.md,**/*.md');
