863e358c142bf7128c1629eb00e512740eea2071
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-06 20:56:40 +0630

    Update invalid-command-option-error.ts

--- src/exceptions/invalid-command-option-error.ts
+++ src/exceptions/invalid-command-option-error.ts
@@ -2,6 +2,6 @@ import { colors } from '../utils/index.js';
 
 export class InvalidCommandOptionError extends Error {
     constructor(message: string, cmdArg: string) {
-        super('Invalid command options - ' + colors.red(cmdArg) + '. ' + message);
+        super(colors.red('Error:') + " Invalid command argument value - '" + colors.red(cmdArg) + "'. " + message);
     }
 }
