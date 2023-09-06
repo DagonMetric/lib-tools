9b0559c8d711ad25d3ae00f11e293724d4bf350b
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-06 17:04:25 +0630

    Add message format to InvalidConfigError

--- src/exceptions/invalid-config-error.ts
+++ src/exceptions/invalid-config-error.ts
@@ -1,5 +1,28 @@
+import { colors } from '../utils/index.js';
+
+function formatMessage(message: string, configFile: string | null, configLocation: string | null): string {
+    let formattedMsg = '';
+
+    if (configFile) {
+        formattedMsg += `${colors.cyan(configFile)}`;
+    }
+
+    if (formattedMsg) {
+        formattedMsg += ' - ';
+    }
+
+    formattedMsg += colors.red('Configuration error') + `: ${message}`;
+
+    if (configLocation) {
+        formattedMsg += ` Config location: `;
+        formattedMsg += colors.red(configLocation);
+    }
+
+    return formattedMsg;
+}
+
 export class InvalidConfigError extends Error {
-    constructor(message: string, configLocation: string | null) {
-        super(`Invalid configuration. ${message} Config location: ${configLocation}`);
+    constructor(message: string, configFile: string | null, configLocation: string | null) {
+        super(formatMessage(message, configFile, configLocation));
     }
 }
