9723f15a26335c07cd0b329c711c6383abef1708
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-08 12:17:47 +0630

    Update config-helpers

--- src/config-helpers/read-lib-config-json-file.ts
+++ src/config-helpers/read-lib-config-json-file.ts
@@ -1,6 +1,6 @@
-import Ajv from 'ajv';
+import Ajv, { Schema } from 'ajv';
 
-import { SchemaValidationError } from '../exceptions/index.js';
+import { ConfigSchemaValidationError } from '../exceptions/index.js';
 import { LibConfig } from '../models/index.js';
 import schema from '../schemas/schema.json' assert { type: 'json' };
 import { readJsonWithComments } from '../utils/index.js';
@@ -9,7 +9,7 @@ const ajv = new Ajv.default({ allErrors: true });
 
 const libConfigCache = new Map<string, LibConfig>();
 
-export async function readLibConfigJsonFile(configPath: string): Promise<LibConfig> {
+export async function readLibConfigJsonFile(configPath: string, configSchema: Schema = schema): Promise<LibConfig> {
     let libConfig = libConfigCache.get(configPath);
     if (libConfig) {
         return libConfig;
@@ -17,12 +17,12 @@ export async function readLibConfigJsonFile(configPath: string): Promise<LibConf
 
     libConfig = (await readJsonWithComments(configPath)) as LibConfig;
 
-    const validate = ajv.compile(schema);
+    const validate = ajv.compile(configSchema);
 
     const valid = validate(libConfig);
 
     if (!valid) {
-        throw new SchemaValidationError(validate.errors ?? [], configPath);
+        throw new ConfigSchemaValidationError(validate.errors ?? [], configPath);
     }
 
     libConfigCache.set(configPath, libConfig);
