08275f3df7f73d704d21bf92ebc1fb2d72c30390
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-12 00:03:19 +0630

    Update utils

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -1,14 +1,25 @@
 import { spawn } from 'node:child_process';
 
+import { Logger } from './logger.js';
+
 export class ExitCodeError extends Error {
     constructor(readonly exitCode: number) {
         super('');
     }
 }
 
-export async function exec(cmd: string): Promise<void> {
+export async function exec(cmd: string, logger: Logger): Promise<void> {
     return new Promise((resolve, reject) => {
-        const proc = spawn(cmd, { stdio: 'inherit', shell: true });
+        // const proc = spawn(cmd, { stdio: 'inherit', shell: true });
+        const proc = spawn(cmd, { shell: true });
+
+        proc.stdout?.on('data', (data) => {
+            logger.info(`${(data as string)?.toString().trimEnd()}`);
+        });
+
+        proc.stderr?.on('data', (data) => {
+            logger.error(`${(data as string)?.toString().trimEnd()}`);
+        });
 
         proc.on('exit', (exitCode) => {
             if (!exitCode || exitCode === 0) {
