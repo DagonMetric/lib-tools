306a3f26a2a91c66c29a2480f280a5e7948b6680
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-17 21:04:58 +0630

    Update tests

--- tests/test-data/style/postcss.config.mjs
+++ tests/test-data/style/postcss.config.mjs
@@ -1,5 +1,20 @@
-export default {
-    plugins: {
-        'postcss-plugin': {}
-    }
-};
+/**
+ * @param {{logLevel: string;env?: string;}} [ctx]
+ */
+export default function (ctx) {
+    return {
+        plugins: {
+            'postcss-preset-env': {
+                debug: ctx.logLevel === 'debug' ? true : false,
+                env: ctx.env,
+                stage: 1,
+                minimumVendorImplementations: 0
+                // browsers: ['last 1 versions and supports es6-module']
+                // autoprefixer: {
+                //     // env: ctx.env, //  (string): environment for Browserslist.,
+                //     grid: true
+                // }
+            }
+        }
+    };
+}
