1d6de692b598a0f158e114eaafee5d07e63b071c
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-19 14:05:42 +0630

    Update tests

--- tests/config-helpers-get-parsed-build-task.test.ts
+++ tests/config-helpers-get-parsed-build-task.test.ts
@@ -4,7 +4,7 @@ import { describe, it } from 'node:test';
 
 import { getParsedBuildTask } from '../src/config-helpers/get-parsed-build-task.js';
 import { BuildTask } from '../src/config-models/index.js';
-import { ParsedBuildTaskConfig, WorkspaceInfo } from '../src/config-models/parsed/index.js';
+import { PackageJsonInfo, ParsedBuildTaskConfig, WorkspaceInfo } from '../src/config-models/parsed/index.js';
 import { InvalidConfigError } from '../src/exceptions/index.js';
 
 void describe('config-helpers/get-parsed-build-task', () => {
@@ -169,6 +169,120 @@ void describe('config-helpers/get-parsed-build-task', () => {
             };
 
             assert.deepStrictEqual(result, expected);
+        });
+
+        void it('should parse build task config with banner file and packageJsonInfo', async () => {
+            const buildTask: BuildTask = {
+                banner: 'banner.txt'
+            };
+
+            const workspaceInfo: WorkspaceInfo = {
+                workspaceRoot: path.resolve(process.cwd(), './tests/test-data/parsing/withconfig'),
+                projectRoot: path.resolve(process.cwd(), './tests/test-data/parsing/withconfig'),
+                projectName: 'test-project',
+                configPath: null,
+                nodeModulePath: null
+            };
+
+            const packageJsonInfo: PackageJsonInfo = {
+                packageJson: {
+                    version: '1.0.0',
+                    author: 'DagonMetric',
+                    license: 'MIT',
+                    homepage: 'https://github.com/dagonmetric/lib-tools',
+                    description: 'This is a test project.'
+                },
+                packageJsonPath: '',
+                packageName: 'package-1',
+                packageNameWithoutScope: '',
+                packageScope: null,
+                newPackageVersion: null,
+                rootPackageJson: null,
+                rootPackageJsonPath: null
+            };
+
+            const result = await getParsedBuildTask(buildTask, workspaceInfo, packageJsonInfo);
+
+            const expected: ParsedBuildTaskConfig = {
+                ...buildTask,
+                _taskName: 'build',
+                _workspaceInfo: workspaceInfo,
+                _packageJsonInfo: packageJsonInfo,
+                _outDir: path.resolve(workspaceInfo.projectRoot, 'dist'),
+                _bannerText: `// DagonMetric, MIT, ${new Date()
+                    .getFullYear()
+                    .toString()}, https://github.com/dagonmetric/lib-tools, test-project, package-1, 1.0.0, This is a test project.`
+            };
+
+            assert.deepStrictEqual(result, expected);
+        });
+
+        void it('should parse build task config with banner=true and packageJsonInfo', async () => {
+            const buildTask: BuildTask = {
+                banner: true
+            };
+
+            const workspaceInfo: WorkspaceInfo = {
+                workspaceRoot: path.resolve(process.cwd(), './tests/test-data/parsing/withconfig'),
+                projectRoot: path.resolve(process.cwd(), './tests/test-data/parsing/withconfig'),
+                projectName: 'test-project',
+                configPath: null,
+                nodeModulePath: null
+            };
+
+            const packageJsonInfo: PackageJsonInfo = {
+                packageJson: {
+                    version: '1.0.0',
+                    author: 'DagonMetric',
+                    license: 'MIT',
+                    homepage: 'https://github.com/dagonmetric/lib-tools',
+                    description: 'This is a test project.'
+                },
+                packageJsonPath: '',
+                packageName: 'package-1',
+                packageNameWithoutScope: '',
+                packageScope: null,
+                newPackageVersion: null,
+                rootPackageJson: null,
+                rootPackageJsonPath: null
+            };
+
+            const result = await getParsedBuildTask(buildTask, workspaceInfo, packageJsonInfo);
+
+            const expected: ParsedBuildTaskConfig = {
+                ...buildTask,
+                _taskName: 'build',
+                _workspaceInfo: workspaceInfo,
+                _packageJsonInfo: packageJsonInfo,
+                _outDir: path.resolve(workspaceInfo.projectRoot, 'dist'),
+                _bannerText: `// DagonMetric, MIT, ${new Date()
+                    .getFullYear()
+                    .toString()}, https://github.com/dagonmetric/lib-tools, test-project, package-1, 1.0.0, This is a test project.`
+            };
+
+            assert.deepStrictEqual(result, expected);
+        });
+
+        void it('should throw an error if banner file not exist', async () => {
+            const buildTask: BuildTask = {
+                banner: 'notexist.txt'
+            };
+
+            const workspaceInfo: WorkspaceInfo = {
+                workspaceRoot: path.resolve(process.cwd(), './tests/test-data/parsing'),
+                projectRoot: path.resolve(process.cwd(), './tests/test-data/parsing'),
+                projectName: null,
+                configPath: null,
+                nodeModulePath: null
+            };
+
+            const expectedError = new InvalidConfigError(
+                `Banner file could not be found.`,
+                workspaceInfo.configPath,
+                `projects/${workspaceInfo.projectName ?? '0'}/build/banner`
+            );
+
+            await assert.rejects(async () => await getParsedBuildTask(buildTask, workspaceInfo, null), expectedError);
         });
 
         void describe('validate outDir', () => {
