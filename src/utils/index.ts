dc57c669d4e3f7005eb9a2778d0bf89858705420
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-30 11:05:17 +0630

    Update utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './colorize.js';
 export * from './dash-case-to-camel-case.js';
 export * from './exec.js';
+export * from './format-size-in-bytes.js';
 export * from './logger.js';
 export * from './path-helpers.js';
 export * from './read-json-with-comments.js';
