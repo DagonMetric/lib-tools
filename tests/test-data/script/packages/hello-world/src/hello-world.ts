565198e10c227d7ff0afb96ae85bf7691e22826f
Moe Myat Zaw <mmzliveid@gmail.com>
2023-09-30 11:05:40 +0630

    Update tests

--- tests/test-data/script/packages/hello-world/src/hello-world.ts
+++ tests/test-data/script/packages/hello-world/src/hello-world.ts
@@ -1,4 +1,13 @@
-export function sayHello() {
+// Banner
+import { Message } from './message';
+
+export const VERSION = '0.0.0-PLACEHOLDER';
+
+/**
+ * sayHello function.
+ * @param message Message object.
+ */
+export function sayHello(message: Message) {
     // eslint-disable-next-line no-console
-    console.log('Hello');
+    console.log(message.text);
 }
