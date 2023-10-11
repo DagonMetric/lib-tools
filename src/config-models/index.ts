8371911cc74bf935c7e79e3b41db93e79efded37
Moe Myat Zaw <mmzliveid@gmail.com>
2023-10-11 19:37:16 +0630

    Update config-models

--- src/config-models/index.ts
+++ src/config-models/index.ts
@@ -1,3 +1,6 @@
-export * from './task-options.js';
 export * from './build-task-config.js';
+export * from './command-options.js';
 export * from './custom-task-config.js';
+export * from './lib-config.js';
+export * from './project-config.js';
+export * from './task-options.js';
