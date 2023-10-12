3c7eaf731fc4d5540f139043fec15c026ddab38e
Moe Myat Zaw <mmzliveid@gmail.com>
2023-10-12 15:34:54 +0630

    Update exceptions

--- src/exceptions/invalid-command-option-error.ts
+++ src/exceptions/invalid-command-option-error.ts
@@ -1,18 +1,30 @@
 import { CommandOptions } from '../config-models/index.js';
 import { colors } from '../utils/index.js';
 
+function formatErrorMessage(
+    argName: keyof CommandOptions,
+    argValue: string | null | undefined,
+    message: string | null | undefined
+): string {
+    let errMessage = colors.lightRed('Error:') + ' Invalid command argument value';
+
+    if (argValue) {
+        errMessage += ` ${colors.lightRed(`${argName}=${argValue}`)}.`;
+    } else {
+        errMessage += ` ${colors.lightRed(`${argName}`)}.`;
+    }
+
+    errMessage += message ? ` ${message}` : '';
+
+    return errMessage;
+}
+
 export class InvalidCommandOptionError extends Error {
     constructor(
         argName: keyof CommandOptions,
         argValue: string | null | undefined,
         message: string | null | undefined
     ) {
-        super(
-            colors.lightRed('Error:') + ' Invalid command argument ' + argValue == null
-                ? ''
-                : 'value ' + argValue == null
-                ? colors.lightRed(`${argName}`)
-                : colors.lightRed(`${argName}=${argValue}`) + '. ' + message ?? ''
-        );
+        super(formatErrorMessage(argName, argValue, message));
     }
 }
