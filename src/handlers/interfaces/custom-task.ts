501ee1613dd508ab6c5b135abeed94bf6290f52e
Moe Myat Zaw <mmzliveid@gmail.com>
2023-10-13 12:26:29 +0630

    Update handler interfaces

--- src/handlers/interfaces/custom-task.ts
+++ src/handlers/interfaces/custom-task.ts
@@ -1,8 +1,8 @@
-import { TaskBaseOptions } from '../../config-models/index.js';
+import { TaskOptions } from '../../config-models/index.js';
 
 import { TaskConfigInfo } from './task-config-info.js';
 
-export interface CustomTask extends Omit<TaskBaseOptions, 'skip'>, TaskConfigInfo {
+export interface CustomTask extends Omit<TaskOptions, 'skip'>, TaskConfigInfo {
     [option: string]: unknown;
     readonly taskCategory: 'custom';
     readonly handler: string;
