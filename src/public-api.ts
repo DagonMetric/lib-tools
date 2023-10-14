bc5cd28475d9fb1cf63dfc677da7caea831265fb
Moe Myat Zaw <mmzliveid@gmail.com>
2023-10-14 17:47:33 +0630

    Update public-api.ts

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,4 +1,4 @@
+export * from './utils/colors.js';
 export * from './utils/logger.js';
-export * from './config-models/task-options.js';
-export * from './config-models/build-task-options.js';
-export * from './config-models/custom-task-options.js';
+export * from './config-models/index.js';
+export * from './handlers/index.js';
