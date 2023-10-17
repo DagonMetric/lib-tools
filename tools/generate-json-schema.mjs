e6edde50333da8bb107672aba92d6b78a7eb1266
Moe Myat Zaw <mmzliveid@gmail.com>
2023-10-17 22:09:30 +0630

    Update tools

--- tools/generate-json-schema.mjs
+++ tools/generate-json-schema.mjs
@@ -6,34 +6,54 @@ import { createGenerator } from 'ts-json-schema-generator';
 
 const thisFile = fileURLToPath(import.meta.url);
 const thisDir = path.dirname(thisFile);
-const schemaOutputFilePath = path.resolve(thisDir, '../dist/schemas/schema.json');
+const defaultSchemaOutFilePath = path.resolve(thisDir, '../dist/schemas/schema.json');
 
-export async function generateJsonSchemaFile() {
+/**
+ * @param {{[key: string]: unknown}} [task]
+ * @param {{logger?: { info: (message: string) => void}}} [options]
+ */
+export async function generateJsonSchema(task, options) {
+    const { logger } = options ?? {};
+    logger?.info('Generating json schema from typescript file...');
+
     const schema = createGenerator({
         // -i, --id
         schemaId: 'lib-tools://schemas/schema.json',
         // -p, --path
-        // path: '../src/config-models/**/*.mts',
+        // path: '../src/config-models/**/*.mts'
         // -f, --tsconfig
         tsconfig: path.resolve(thisDir, './tsconfig.schema.json')
         // -t, --type
-        // type: 'LibConfig'
+        // type: '*'
     }).createSchema('LibConfig');
 
     const schemaString = JSON.stringify(schema, null, 2);
 
-    const schemaOutputDir = path.dirname(schemaOutputFilePath);
+    logger?.info('Success.');
 
-    await fs.mkdir(schemaOutputDir, {
+    const { projectRoot, outFile } = task ?? {};
+
+    let schemaOutFilePath;
+    if (projectRoot && outFile) {
+        schemaOutFilePath = path.relative(projectRoot, outFile);
+    } else {
+        schemaOutFilePath = defaultSchemaOutFilePath;
+    }
+
+    logger?.info(
+        `Writing schema content to ${path.relative(process.cwd(), schemaOutFilePath).replace(/\\/g, '/')} ...`
+    );
+
+    await fs.mkdir(path.dirname(schemaOutFilePath), {
         mode: 0o777,
         recursive: true
     });
 
-    await fs.writeFile(schemaOutputFilePath, schemaString);
+    await fs.writeFile(schemaOutFilePath, schemaString, 'utf-8');
+
+    logger?.info('Success.');
 }
 
-export default generateJsonSchemaFile;
-
 if (process.argv.length >= 2 && process.argv[1] === path.resolve(thisFile)) {
-    await generateJsonSchemaFile();
+    await generateJsonSchema();
 }
